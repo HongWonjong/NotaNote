eb0797bb34fbf48e7fab70249e790238bfc4ce88
hippieperm <leess0133@gmail.com>
2025-06-02 12:19:08 +0900

    메인 아이템 위젯의 레이아웃을 수정하여 패딩을 추가하고, 제목 텍스트 스타일을 변경하며, 아이콘을 추가하여 디자인을 개선함

--- lib/pages/main_page/widgets/main_item.dart
+++ lib/pages/main_page/widgets/main_item.dart
@@ -4,26 +4,31 @@ class MainItem extends StatelessWidget {
   final String title;
 
   const MainItem({
+    Key? key,
     required this.title,
-    super.key,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 62,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
       decoration: BoxDecoration(
-        color: Color(0xffF4F4F4),
-        borderRadius: BorderRadius.circular(16),
+        color: const Color(0xFFF2F2F2),
+        borderRadius: BorderRadius.circular(12),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 17),
-            child: Text(title),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Icon(
+            Icons.more_horiz,
+            color: Color(0xFFB5B5B5),
           ),
         ],
       ),
