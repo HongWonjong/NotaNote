e202c3390a927ee7d6b084e02c0e4812c89a5d1f
hippieperm <leess0133@gmail.com>
2025-06-02 12:03:40 +0900

    메인 페이지에 새로운 위젯 'MainItem' 추가: 제목을 위한 속성 포함

---
+++ lib/pages/main_page/widgets/main_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainItem extends StatelessWidget {
+  final String title;
+
+  const MainItem({
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
