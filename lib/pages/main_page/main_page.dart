d4ed5870959d1064110bff855eb485db9893c546
hippieperm <leess0133@gmail.com>
2025-06-02 11:40:09 +0900

    메인 페이지 위젯 추가: 앱바에 검색 및 알림 아이콘 버튼 포함, 중앙에 '메인 페이지' 텍스트 표시

---
+++ lib/pages/main_page/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_none),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('메인 페이지'),
+      ),
+    );
+  }
+}
