ff94cc0890fe0e2f142fe43b2efa93db46fc058e
wonjonghong <wonhong1996@naver.com>
2025-06-09 10:57:33 +0900

    fix: 녹음된 오디오 파일에 대한 다양한 옵션을 UI에 반영

--- lib/pages/memo_page/widgets/recording_controller_box.dart
+++ lib/pages/memo_page/widgets/recording_controller_box.dart
@@ -4,145 +4,294 @@ import 'package:nota_note/viewmodels/recording_viewmodel.dart';
 import 'package:nota_note/providers/recording_box_visibility_provider.dart';
 import 'package:intl/intl.dart';
 
-class RecordingControllerBox extends ConsumerWidget {
+class RecordingControllerBox extends ConsumerStatefulWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _RecordingControllerBoxState createState() => _RecordingControllerBoxState();
+}
+
+class _RecordingControllerBoxState extends ConsumerState<RecordingControllerBox> {
+  bool _isMenuVisible = false;
+  String _selectedLanguage = '한국어';
+
+  @override
+  Widget build(BuildContext context) {
     final recordingState = ref.watch(recordingViewModelProvider);
     final recordingViewModel = ref.read(recordingViewModelProvider.notifier);
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Material(
-      borderRadius: BorderRadius.circular(8.0),
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxWidth: screenWidth * 0.9,
-          maxHeight: 60,
-        ),
-        child: Container(
-          padding: EdgeInsets.all(12.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-            border: Border.all(
-              color: Colors.grey[400]!,
-              width: 1.0,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Material(
+          borderRadius: BorderRadius.circular(8.0),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: screenWidth * 0.9,
+              maxHeight: 60,
             ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: recordingState.recordings.isNotEmpty
-                          ? LayoutBuilder(
-                        builder: (context, constraints) {
-                          final recording = recordingState.recordings.last;
-                          return Container(
-                            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            child: Consumer(
-                              builder: (context, ref, child) {
-                                final state = ref.watch(recordingViewModelProvider);
-                                final isPlaying = state.currentlyPlayingPath == recording.path;
-                                return Row(
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(
-                                        isPlaying ? Icons.stop : Icons.play_arrow,
-                                        size: 20.0,
-                                      ),
-                                      padding: EdgeInsets.zero,
-                                      constraints: BoxConstraints(),
-                                      onPressed: () {
-                                        recordingViewModel.playRecording(recording.path);
-                                      },
-                                    ),
-                                    SizedBox(width: 4.0),
-                                    Text(
-                                      '${recording.duration.inMinutes.toString().padLeft(2, '0')}:${(recording.duration.inSeconds % 60).toString().padLeft(2, '0')}',
-                                      style: TextStyle(fontSize: 14.0),
-                                    ),
-                                  ],
-                                );
-                              },
-                            ),
-                          );
-                        },
-                      )
-                          : Center(
-                        child: Text(
-                          '녹음된 파일이 없습니다.',
-                          style: TextStyle(fontSize: 12.0, color: Colors.grey),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.more_horiz, size: 20.0),
-                      padding: EdgeInsets.zero,
-                      constraints: BoxConstraints(),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: Text('옵션'),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                if (recordingState.recordings.isNotEmpty)
-                                  ListTile(
-                                    title: Text('다운로드'),
-                                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                                    onTap: () {
-                                      recordingViewModel.downloadRecording(recordingState.recordings.last.path);
-                                      Navigator.pop(context);
-                                    },
-                                  ),
-                                if (recordingState.recordings.isNotEmpty)
-                                  ListTile(
-                                    title: Text('삭제'),
-                                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                                    onTap: () {
-                                      recordingViewModel.deleteRecording(recordingState.recordings.last.path);
-                                      Navigator.pop(context);
-                                      if (ref.read(recordingViewModelProvider).recordings.isEmpty) {
-                                        ref.read(recordingBoxVisibilityProvider.notifier).state = false;
-                                      }
-                                    },
-                                  ),
-                                ListTile(
-                                  title: Text('닫기'),
-                                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                                  onTap: () {
-                                    ref.read(recordingBoxVisibilityProvider.notifier).state = false;
-                                    Navigator.pop(context);
+            child: Container(
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: Colors.grey[400]!,
+                  width: 1.0,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: recordingState.recordings.isNotEmpty
+                              ? LayoutBuilder(
+                            builder: (context, constraints) {
+                              final recording = recordingState.recordings.last;
+                              return Container(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Consumer(
+                                  builder: (context, ref, child) {
+                                    final state = ref.watch(recordingViewModelProvider);
+                                    final isPlaying = state.currentlyPlayingPath == recording.path;
+                                    return Row(
+                                      children: [
+                                        IconButton(
+                                          icon: Icon(
+                                            isPlaying ? Icons.stop : Icons.play_arrow,
+                                            size: 20.0,
+                                          ),
+                                          padding: EdgeInsets.zero,
+                                          constraints: BoxConstraints(),
+                                          onPressed: () {
+                                            recordingViewModel.playRecording(recording.path);
+                                          },
+                                        ),
+                                        SizedBox(width: 4.0),
+                                        Text(
+                                          '${recording.duration.inMinutes.toString().padLeft(2, '0')}:${(recording.duration.inSeconds % 60).toString().padLeft(2, '0')}',
+                                          style: TextStyle(fontSize: 14.0),
+                                        ),
+                                      ],
+                                    );
                                   },
                                 ),
-                              ],
+                              );
+                            },
+                          )
+                              : Center(
+                            child: Text(
+                              '녹음된 파일이 없습니다.',
+                              style: TextStyle(fontSize: 12.0, color: Colors.grey),
                             ),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text('취소'),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.more_horiz, size: 20.0),
+                          padding: EdgeInsets.zero,
+                          constraints: BoxConstraints(),
+                          onPressed: () {
+                            setState(() {
+                              _isMenuVisible = !_isMenuVisible;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        if (_isMenuVisible)
+          Positioned(
+            right: 0,
+            bottom: 62,
+            child: Material(
+              borderRadius: BorderRadius.circular(8.0),
+              elevation: 2.0,
+              child: Container(
+                width: 160,
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  border: Border.all(
+                    color: Colors.grey[400]!,
+                    width: 1.0,
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.language, size: 16.0),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: DropdownButton<String>(
+                              isExpanded: true,
+                              value: _selectedLanguage,
+                              items: ['한국어', '영어']
+                                  .map((language) => DropdownMenuItem(
+                                value: language,
+                                child: Text(
+                                  language,
+                                  style: TextStyle(fontSize: 14.0),
+                                ),
+                              ))
+                                  .toList(),
+                              onChanged: (value) {
+                                setState(() {
+                                  _selectedLanguage = value!;
+                                  _isMenuVisible = false;
+                                });
+                              },
+                              underline: SizedBox(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    if (recordingState.recordings.isNotEmpty)
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          // TODO: Implement convert to text
+                          setState(() {
+                            _isMenuVisible = false;
+                          });
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.text_snippet, size: 16.0),
+                              SizedBox(width: 8.0),
+                              Text(
+                                '텍스트로 변환',
+                                style: TextStyle(fontSize: 14.0),
                               ),
                             ],
                           ),
-                        );
+                        ),
+                      ),
+                    if (recordingState.recordings.isNotEmpty)
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          recordingViewModel.downloadRecording(recordingState.recordings.last.path);
+                          setState(() {
+                            _isMenuVisible = false;
+                          });
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.download, size: 16.0),
+                              SizedBox(width: 8.0),
+                              Text(
+                                '다운로드',
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    if (recordingState.recordings.isNotEmpty)
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          // TODO: Implement recording history
+                          setState(() {
+                            _isMenuVisible = false;
+                          });
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.history, size: 16.0),
+                              SizedBox(width: 8.0),
+                              Text(
+                                '녹음기록',
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    if (recordingState.recordings.isNotEmpty)
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          recordingViewModel.deleteRecording(recordingState.recordings.last.path);
+                          if (ref.read(recordingViewModelProvider).recordings.isEmpty) {
+                            ref.read(recordingBoxVisibilityProvider.notifier).state = false;
+                          }
+                          setState(() {
+                            _isMenuVisible = false;
+                          });
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.delete, size: 16.0),
+                              SizedBox(width: 8.0),
+                              Text(
+                                '삭제',
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    GestureDetector(
+                      behavior: HitTestBehavior.opaque,
+                      onTap: () {
+                        ref.read(recordingBoxVisibilityProvider.notifier).state = false;
+                        setState(() {
+                          _isMenuVisible = false;
+                        });
                       },
+                      child: Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.close, size: 16.0),
+                            SizedBox(width: 8.0),
+                            Text(
+                              '닫기',
+                              style: TextStyle(fontSize: 14.0),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+      ],
     );
   }
 }
