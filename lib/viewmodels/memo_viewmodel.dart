02b16efc6e3b4e57910524319940547cbf0a1172
wonjonghong <wonhong1996@naver.com>
2025-06-11 21:16:48 +0900

    feat: 메모장에서 수정 후 1.5초가 지나거나 메모장 페이지를 나갈 때마다 저장을 하는 기존 로직에, 제목 업데이트 로직을 추가& 메모 뷰 모델에 제목 업데이트 메서드 추가

--- lib/viewmodels/memo_viewmodel.dart
+++ lib/viewmodels/memo_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nota_note/models/memo.dart';
+import 'package:uuid/uuid.dart';
 
 final memoViewModelProvider =
 Provider.family<MemoViewModel, String>((ref, groupId) => MemoViewModel(ref, groupId));
@@ -21,6 +22,46 @@ class MemoViewModel {
         .map((snapshot) => snapshot.docs
         .map((doc) => Memo.fromFirestore(doc, _groupId))
         .toList());
+  }
+
+  Future<String?> addMemo() async {
+    try {
+      final noteId = const Uuid().v4();
+      final now = DateTime.now();
+      final docRef = _firestore
+          .collection('notegroups')
+          .doc(_groupId)
+          .collection('notes')
+          .doc(noteId);
+
+      await docRef.set({
+        'title': '제목 없음',
+        'tags': [],
+        'createdAt': Timestamp.fromDate(now),
+        'updatedAt': Timestamp.fromDate(now),
+        'pageId': '1',
+      });
+
+      return noteId;
+    } catch (e) {
+      throw Exception("메모 추가 중 오류가 발생했습니다: $e");
+    }
+  }
+
+  Future<void> updateMemoTitle(String noteId, String title) async {
+    try {
+      final docRef = _firestore
+          .collection('notegroups')
+          .doc(_groupId)
+          .collection('notes')
+          .doc(noteId);
+      await docRef.update({
+        'title': title,
+        'updatedAt': Timestamp.fromDate(DateTime.now()),
+      });
+    } catch (e) {
+      throw Exception("메모 제목 업데이트 중 오류가 발생했습니다: $e");
+    }
   }
 
   Future<void> deleteMemos(List<String> noteIds) async {
