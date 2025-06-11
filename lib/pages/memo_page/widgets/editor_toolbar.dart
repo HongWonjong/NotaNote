63c4c6330da44bcd7fbe5cba0644d24e7afd4d7f
wonjonghong <wonhong1996@naver.com>
2025-06-11 14:59:44 +0900

    feat: svg 아이콘 적용

--- lib/pages/memo_page/widgets/editor_toolbar.dart
+++ lib/pages/memo_page/widgets/editor_toolbar.dart
@@ -8,6 +8,7 @@ import 'package:intl/intl.dart';
 import 'package:nota_note/providers/recording_box_visibility_provider.dart';
 import 'package:nota_note/viewmodels/image_upload_viewmodel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class EditorToolbar extends ConsumerStatefulWidget {
@@ -275,7 +276,7 @@ class _EditorToolbarState extends ConsumerState<EditorToolbar> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 IconButton(
-                  icon: recordingState.isRecording ? Icon(Icons.stop) : FaIcon(FontAwesomeIcons.microphone),
+                  icon: recordingState.isRecording ? SvgPicture.asset('assets/icons/Stop.svg') : SvgPicture.asset('assets/icons/Mic.svg'),
                   onPressed: () async {
                     final recordingViewModel = ref.read(recordingViewModelProvider.notifier);
                     if (recordingState.isRecording) {
@@ -301,7 +302,7 @@ class _EditorToolbarState extends ConsumerState<EditorToolbar> {
               ],
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.camera),
+              icon: SvgPicture.asset('assets/icons/Camera.svg'),
               onPressed: () {
                 ref.read(imageUploadProvider({
                   'groupId': widget.groupId,
@@ -323,38 +324,38 @@ class _EditorToolbarState extends ConsumerState<EditorToolbar> {
               },
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.link),
+              icon: SvgPicture.asset('assets/icons/Link.svg'),
               onPressed: () {},
             ),
             CompositedTransformTarget(
               link: _layerLink,
               child: IconButton(
-                icon: Image.asset('assets/images/font_size.png', width: 24.0, height: 24.0),
+                icon: SvgPicture.asset('assets/icons/Fontsize.svg'),
                 onPressed: () => _toggleDropdown(context),
               ),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.bold),
+              icon: SvgPicture.asset('assets/icons/Bold.svg'),
               color: _isFormatActive(Attribute.bold) ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleFormat(Attribute.bold),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.italic),
+              icon: SvgPicture.asset('assets/icons/Italic.svg'),
               color: _isFormatActive(Attribute.italic) ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleFormat(Attribute.italic),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.underline),
+              icon: SvgPicture.asset('assets/icons/Underline.svg'),
               color: _isFormatActive(Attribute.underline) ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleFormat(Attribute.underline),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.strikethrough),
+              icon: SvgPicture.asset('assets/icons/TextStrikethrough.svg'),
               color: _isFormatActive(Attribute.strikeThrough) ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleFormat(Attribute.strikeThrough),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.palette),
+              icon: SvgPicture.asset('assets/icons/Palette.svg'),
               onPressed: () {},
             ),
             IconButton(
@@ -362,46 +363,46 @@ class _EditorToolbarState extends ConsumerState<EditorToolbar> {
               onPressed: () {},
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.highlighter),
+              icon: SvgPicture.asset('assets/icons/TextAlignLeft.svg'),
               color: _isAlignActive('left') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleAlign('left'),
             ),
             IconButton(
-              icon: Icon(Icons.format_align_center),
+              icon: SvgPicture.asset('assets/icons/TextAlignCenter.svg'),
               color: _isAlignActive('center') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleAlign('center'),
             ),
             IconButton(
-              icon: Icon(Icons.format_align_right),
+              icon: SvgPicture.asset('assets/icons/TextAlignRight.svg'),
               color: _isAlignActive('right') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleAlign('right'),
             ),
             IconButton(
-              icon: Icon(Icons.format_align_justify),
+              icon: SvgPicture.asset('assets/icons/TextAlignJustify.svg'),
               color: _isAlignActive('justify') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleAlign('justify'),
             ),
             IconButton(
-              icon: Icon(Icons.format_list_numbered),
+              icon: SvgPicture.asset('assets/icons/NumberStyleTable.svg'),
               color: _isListActive('ordered') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleList('ordered'),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.highlighter),
+              icon: SvgPicture.asset('assets/icons/DotStyleTable.svg'),
               color: _isListActive('bullet') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleList('bullet'),
             ),
             IconButton(
-              icon: Icon(Icons.check_box_outlined),
+              icon: SvgPicture.asset('assets/icons/CheckStyleTable.svg'),
               color: _isListActive('checked') ? Color(0xFF61CFB2) : null,
               onPressed: () => _toggleList('checked'),
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.code),
+              icon: SvgPicture.asset('assets/icons/Code.svg'),
               onPressed: _insertCodeBlock,
             ),
             IconButton(
-              icon: FaIcon(FontAwesomeIcons.table),
+              icon: SvgPicture.asset('assets/icons/Table.svg'),
               onPressed: () {},
             ),
             IconButton(
