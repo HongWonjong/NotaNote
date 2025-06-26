5b7e514aa14eac71c094b1167dd96a63ab931c14
kdy5487 <kdykdy5487@naver.com>
2025-06-26 16:24:02 +0900

    fix: 코드제거

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import KakaoSDKAuth
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,12 +10,5 @@ import KakaoSDKAuth
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-   // 카카오톡으로 로그인을 위한 설정
-  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-      if (AuthApi.isKakaoTalkLoginUrl(url)) {
-          return AuthApi.handleOpenUrl(url: url)
-      }
-      // 다른 플러그인(구글, 파이어베이스 등등) 처리를 위해 super 유지
-      return super.application(app, open: url, options: options)
-  }
+ 
 }
