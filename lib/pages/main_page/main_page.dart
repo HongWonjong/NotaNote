e414697a598b2ecb992469e012e5fbb29843d8f5
wonjonghong <wonhong1996@naver.com>
2025-06-29 21:11:34 +0900

    feat: 메인 페이지 서랍 메뉴에서 해시태그를 볼 수 있도록 임시 배치(추후 디자이너와 협의 후 수정할 것)

--- lib/pages/main_page/main_page.dart
+++ lib/pages/main_page/main_page.dart
@@ -378,6 +378,16 @@ class _MainPageState extends ConsumerState<MainPage>
                           ),
                           const SizedBox(height: 2),
                           Text(
+                            user?.hashTag ?? '@해시태그',
+                            style: TextStyle(
+                              color: Color(0xFF666666),
+                              fontSize: 12,
+                              fontFamily: 'Pretendard',
+                              height: 1.5,
+                            ),
+                          ),
+                          const SizedBox(height: 2),
+                          Text(
                             user?.email ?? '이메일',
                             style: TextStyle(
                               color: Color(0xFF666666),
@@ -571,9 +581,9 @@ class _MainPageState extends ConsumerState<MainPage>
       List<SharedGroupWithRole> sharedGroupsWithRole,
       bool isLoading,
       String? error,
-      String? userId,
-      ) {
-    final invitationCount = ref.watch(notificationViewModelProvider).invitationCount;
+      String? userId) {
+    final invitationCount =
+        ref.watch(notificationViewModelProvider).invitationCount;
 
     return Scaffold(
       body: Column(
